f751a74e899bd05c2fc2950037fd33e603e4d0a4
Will Ramirez <wramirez09@users.noreply.github.com>
2017-10-03 13:48:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,16 +4,6 @@ var app = express();
 
 app.use(express.static('src'));
 
-// app.all("/phantom-capture/", api.phantomscreencapture.bind(this));
-
-// /**
-//  * @param {index point} does nothing.
-//  */
-
-// app.all("/", api.postIndex.bind(this));
-
-
-
 app.listen(3100, function(req, res) {
     console.log('app listening on port 3100!')
 });
