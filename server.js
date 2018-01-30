9f55ca38fd7fa8d0e84fb529f83ea351c9bac3f8
Ramirez William <william.ramirez@designory.com>
2018-01-30 10:57:53 -0600

    -remove colors

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ var app = express();
 app.use(express.static('src'));
 
 app.listen(process.env.PORT || 3180, () => {
-    console.log(colors.green(`Listening on port ${process.env.PORT || 3180}`));
+    console.log(`Listening on port ${process.env.PORT || 3180}`);
 });
