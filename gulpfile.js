f538b98bfc3cdc6cf7d35fead2d06a7fa3f7c392
Ramirez William <william.ramirez@designory.com>
2018-05-08 16:42:43 -0500

    added gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('default', function () {
     console.log("gulp installed")
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./build/css/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./build/css/**/*.scss', ['sass']);
 });
